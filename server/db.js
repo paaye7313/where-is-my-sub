eede265803f9640f016b73821a08313ec78a1266
paaye7313 <kig7313@naver.com>
2026-05-06 18:00:25 +0900

    chore : Render DB 연결 설정

--- server/db.js
+++ server/db.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-})
+const pool = process.env.DB_URL
+  ? new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false },
+  })
+  : new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  })
 
 module.exports = pool
